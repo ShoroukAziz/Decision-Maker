35d9685549459638a6cdf3a0446091fc34a27b58
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-12 17:01:54 -0700

    🛠  refactor foreign keys data type

--- db/schema/03_options.sql
+++ db/schema/03_options.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS options CASCADE;
 CREATE TABLE options (
   id SERIAL PRIMARY KEY NOT NULL,
-  poll_id INTEGER REFERENCES polls(id),
+  poll_id INTEGER REFERENCES polls(id) ON DELETE CASCADE,
   description TEXT NOT NULL
 );
