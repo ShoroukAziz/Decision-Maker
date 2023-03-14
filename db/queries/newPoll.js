6f923301cd425a00d398ddf6d6879f5033bf599b
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-14 15:47:48 -0600

    ➕ Query to insert new poll and its options

---
+++ db/queries/newPoll.js
@@ -0,0 +1,45 @@
+const db = require('../connection');
+
+const createPoll = function (creatorId, poll) {
+  let queryString = `
+
+  WITH new_poll AS(
+
+    INSERT INTO polls
+     (creator_id,title,question,date_completed)
+     VALUES($1,$2,$3, NULL)
+     RETURNING id
+  )
+
+  INSERT INTO options (poll_id, description)
+    VALUES
+  `;
+
+  poll.options.forEach((option, index) => {
+    queryString += `
+     ((SELECT id FROM new_poll) , $${index + 4} )
+    `;
+    if (index < poll.options.length - 1) {
+      queryString += ', '
+    }
+  });
+
+  queryString += ';'
+
+  const values = [creatorId, poll.title, poll.question].concat(poll.options);
+  // console.log(queryString);
+  // console.log('---------------');
+  // console.log("values", values);
+
+  return db.query(queryString, values)
+    .then((data) => {
+      return;
+    })
+    .catch((err) => {
+      console.log('error:', err.message);
+    });
+};
+
+
+
+module.exports = { createPoll };
