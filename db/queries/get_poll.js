94dbc38251f27862aa5149854e2e3fec0cad3bee
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-14 16:55:13 -0600

    ➕ Query to get a single poll and its options

---
+++ db/queries/get_poll.js
@@ -0,0 +1,29 @@
+const db = require('../connection');
+
+const getPollAndOptionsByPollId = function (pollId) {
+
+  // SELECT polls JOIN options JOIN users(poll_id, poll title, option_description, options, creator_email)
+  const queryString = `
+    SELECT polls.id, title, question ,  users.email, options.id as option_id , options.description
+    FROM polls
+    JOIN options
+    ON  polls.id = options.poll_id
+    JOIN users
+    ON polls.creator_id = users.id
+    WHERE polls.id = $1;
+  `;
+  const values = [pollId];
+
+  return db.query(queryString, values)
+    .then((data) => {
+      return data.rows;
+    })
+    .catch((err) => {
+      console.log('error:', err.message);
+    });
+
+};
+
+
+module.exports = { getPollAndOptionsByPollId };
+
