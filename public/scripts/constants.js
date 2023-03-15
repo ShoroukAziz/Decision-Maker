8170dfa65eee654208d4409ad47025edfb8923ca
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-14 23:49:08 -0600

    ➕ Cconfermation messages

--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -10,6 +10,8 @@ const systemMessages = {
   emptyTitleError: 'Your poll title can\'t be empty!',
   emptyQuestionError: 'Your poll question can\'t be empty!',
   lessThanTwoOptionsError: 'You should have at least two choices!',
-  emptyChoiceFeildsError: 'Your choices can\'t be empty'
+  emptyChoiceFeildsError: 'Your choices can\'t be empty',
+  confirmPollSubmission: 'Do you really want to submit the poll? You can\'t edit it later',
+  confirmVote: 'Is this your final opinion? You can\'t change it later'
 };
 
