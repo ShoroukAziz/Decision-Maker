e23c58150d9c4a4e0ef1d7ebb2f1e7229c573da0
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-14 22:57:10 -0700

    ➕ implement completePoll function

---
+++ db/queries/polls-complete.js
@@ -0,0 +1,19 @@
+const db = require('../connection');
+
+const completePoll = function (poll_id) {
+  const queryString = `
+  UPDATE polls
+  SET complete = $1
+  WHERE id = $2;`;
+  const values = ['true', poll_id];
+
+  return db.query(queryString, values)
+    .then((data) => {
+      return data.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+module.exports = { completePoll };
