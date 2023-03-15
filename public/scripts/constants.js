5d69398cfe64c39937d5d9405aa6ce5c298dacf9
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-14 23:29:44 -0600

    🛠 update messages & ➕ min choices allowed

--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -2,10 +2,14 @@
  * @fileoverview Conatines all the constant variables of the system for easier mainainance
  */
 
+const MIN_CHOICES_COUNT = 2;
+
 const systemMessages = {
 
   emptyVoterNameError: 'Your name can\'t be empty!',
-  emptyTitleOrQuestionError: 'Your poll title and question can\'t be empty',
-  lessThanTwoOptionsError: 'You should have at least two choices'
-
+  emptyTitleError: 'Your poll title can\'t be empty!',
+  emptyQuestionError: 'Your poll question can\'t be empty!',
+  lessThanTwoOptionsError: 'You should have at least two choices!',
+  emptyChoiceFeildsError: 'Your choices can\'t be empty'
 };
+
