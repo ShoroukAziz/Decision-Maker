b5219d67cefcd332efc8beb18857861744df1350
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-14 22:15:38 -0600

    ➕ file for all constants

---
+++ public/scripts/constants.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Conatines all the constant variables of the system for easier mainainance
+ */
+
+const systemMessages = {
+
+  emptyVoterNameError: 'Your name can\'t be empty!',
+  emptyTitleOrQuestionError: 'Your poll title and question can\'t be empty',
+  lessThanTwoOptionsError: 'You should have at least two choices'
+
+};
